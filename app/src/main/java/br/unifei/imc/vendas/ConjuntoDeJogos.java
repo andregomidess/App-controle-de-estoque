5d42ad4b2ce9a3614c797964705473c377834b5e
andregomidess <andreluccagomides@gmail.com>
2022-12-04 22:41:28 -0300

    Colocando mensagem para edição e exclusão

--- app/src/main/java/br/unifei/imc/vendas/ConjuntoDeJogos.java
+++ app/src/main/java/br/unifei/imc/vendas/ConjuntoDeJogos.java
@@ -1,31 +1,27 @@
-//package br.unifei.imc.vendas;
-//
-//import br.unifei.imc.jogos.Games;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class ConjuntoDeJogos {
-//
-////////////////////////////////////////////
-////Pensando em usar, tipo uma Box já montada
-////////////////////////////////////////////
-//
-//
-//    public List<Games> jogosDeAnimes(){
-//        List<Games> jogos = new ArrayList<>();
-//        jogos.add(new Games("Naruto Shippuden: Ultimate Ninja Storm 4",99.9,"Jogos de animes","CyberConect2"));
-//        jogos.add(new Games("My Hero One's Justice 2",249.9,"Jogos de animes","Bandai Namco Entertainment"));
-//        jogos.add(new Games("Dragon Ball Xenoverse 2",60.99,"Jogos de animes","Dimps"));
-//        return jogos;
-//    }
-//
-//    public List<Games> jogosDeTerror(){
-//        List<Games> jogos = new ArrayList<>();
-//        jogos.add(new Games("Outlast",104.9,"Jogos de Terror","Red Barrels"));
-//        jogos.add(new Games("Outlast2",160.7,"Jogos de Terror","Red Barrels"));
-//        jogos.add(new Games("Amnesia: The Dark Descent",36.99,"Jogos de Terror","Frictional Games"));
-//        return jogos;
-//    }
-//
-//}
+package br.unifei.imc.vendas;
+
+import br.unifei.imc.jogos.Jogo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConjuntoDeJogos {
+
+//////////////////////////////////////////
+//Pensando em usar, tipo uma Box já montada
+//////////////////////////////////////////
+
+
+    public List<Jogo> jogosDeAnimes(){
+        List<Jogo> jogos = new ArrayList<>();
+
+        return jogos;
+    }
+
+    public List<Jogo> jogosDeTerror(){
+        List<Jogo> jogos = new ArrayList<>();
+
+        return jogos;
+    }
+
+}
